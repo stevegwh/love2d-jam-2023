8db31b2e96103228994249605110057c4f4cf9d9
Steven Wheeler <steven.wheeler@pg.canterbury.ac.nz>
2023-02-19 22:25:38 +1300

    first commit

---
+++ conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+    t.console = false
+    t.window.width = 1024
+    t.window.height = 768
+    t.window.title = "Love2D Jam"
+    t.window.vsync = 0
+end
---
+++ main.lua
@@ -0,0 +1,9 @@
+function love.setup()
+end
+
+function love.draw()
+    love.graphics.print('Hello World!', 400, 300)
+end
+
+function love.update()
+end
