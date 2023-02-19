48365d44841d414dfef6f34bd743d5350197c382
Steven Wheeler <steven.wheeler@pg.canterbury.ac.nz>
2023-02-19 23:47:06 +1300

    Added libraries and more boiler plate

--- main.lua
+++ main.lua
@@ -1,4 +1,25 @@
+class = require 'lib.middleclass'
+local _baton = require 'lib.baton'
 function love.setup()
+
+end
+
+function love.load()
+    Input = _baton.new {
+        controls = {
+            left = { 'key:left', 'key:a', 'axis:leftx-', 'button:dpleft' },
+            right = { 'key:right', 'key:d', 'axis:leftx+', 'button:dpright' },
+            up = { 'key:up', 'key:w', 'axis:lefty-', 'button:dpup' },
+            down = { 'key:down', 'key:s', 'axis:lefty+', 'button:dpdown' },
+            action = { 'key:space', 'button:a' },
+            bomb = { 'key:e', 'button:y' },
+            shift = { 'key:lshift', 'button:x' },
+            pause = { 'key:k' },
+        },
+        pairs = {
+            move = { 'left', 'right', 'up', 'down' }
+        },
+    }
 end
 
 function love.draw()
@@ -6,4 +27,10 @@ function love.draw()
 end
 
 function love.update()
+end
+
+function love.keypressed(key, u)
+    if key == "escape" then --set to whatever key you want to use
+        love.event.quit()
+    end
 end
